1507bb130b338abda171564fa06588fc413bc566
Nicholas Oliver Bahary <nicholashendrata110900@gmail.com>
2024-09-18 00:10:55 +0700

    'go to references' to appear in telescope window

--- nvim/lua/keybinds/lsp.lua
+++ nvim/lua/keybinds/lsp.lua
@@ -1,27 +1,29 @@
 -- See ':help vim.lsp.*' for documentation on the below functions.
 
+local telescope = require("telescope.builtin")
+
 local function format_file()
-  vim.lsp.buf.format({ async = true })
+	vim.lsp.buf.format({ async = true })
 end
 
 vim.api.nvim_create_autocmd("LspAttach", {
-  group = vim.api.nvim_create_augroup("UserLspConfig", {}),
-  callback = function(e)
-    -- Quick lookups.
-    vim.keymap.set("n", "<C-space>", vim.lsp.buf.hover, { buffer = e.buf })
-    vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, { buffer = e.buf })
+	group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+	callback = function(e)
+		-- Quick lookups.
+		vim.keymap.set("n", "<C-space>", vim.lsp.buf.hover, { buffer = e.buf })
+		vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, { buffer = e.buf })
 
-    -- Code formatting via none-ls.
-    vim.keymap.set("n", "<leader>ff", format_file, { buffer = e.buf, desc = "Format File" })
+		-- Code formatting via none-ls.
+		vim.keymap.set("n", "<leader>ff", format_file, { buffer = e.buf, desc = "Format File" })
 
-    -- Gotos.
-    vim.keymap.set("n", "gd", vim.lsp.buf.definition, { buffer = e.buf, desc = "Go to Definition" })
-    vim.keymap.set("n", "gD", vim.lsp.buf.declaration, { buffer = e.buf, desc = "Go to Declaration" })
-    vim.keymap.set("n", "gi", vim.lsp.buf.implementation, { buffer = e.buf, desc = "Go to Implementation" })
-    vim.keymap.set("n", "gr", vim.lsp.buf.references, { buffer = e.buf, desc = "Go to References" })
-    vim.keymap.set("n", "gt", vim.lsp.buf.type_definition, { buffer = e.buf, desc = "Go to Type Definition" })
+		-- Gotos.
+		vim.keymap.set("n", "gd", vim.lsp.buf.definition, { buffer = e.buf, desc = "Go to Definition" })
+		vim.keymap.set("n", "gD", vim.lsp.buf.declaration, { buffer = e.buf, desc = "Go to Declaration" })
+		vim.keymap.set("n", "gi", vim.lsp.buf.implementation, { buffer = e.buf, desc = "Go to Implementation" })
+		vim.keymap.set("n", "gr", telescope.lsp_references, { buffer = e.buf, desc = "Go to References" })
+		vim.keymap.set("n", "gt", vim.lsp.buf.type_definition, { buffer = e.buf, desc = "Go to Type Definition" })
 
-    vim.keymap.set({ "n", "v" }, "<leader>c", vim.lsp.buf.code_action, { buffer = e.buf, desc = "Code Actions" })
-    vim.keymap.set("n", "<leader>r", vim.lsp.buf.rename, { buffer = e.buf, desc = "Rename Instances" })
-  end,
+		vim.keymap.set({ "n", "v" }, "<leader>c", vim.lsp.buf.code_action, { buffer = e.buf, desc = "Code Actions" })
+		vim.keymap.set("n", "<leader>r", vim.lsp.buf.rename, { buffer = e.buf, desc = "Rename Instances" })
+	end,
 })
