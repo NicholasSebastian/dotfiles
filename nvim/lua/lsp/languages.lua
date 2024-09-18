de3fd7ca4f455761bb073d8cc603749f6f48c9ed
Nicholas Oliver Bahary <nicholashendrata110900@gmail.com>
2024-09-19 01:23:22 +0700

    split file into 2

---
+++ nvim/lua/lsp/languages.lua
@@ -0,0 +1,26 @@
+-- Packages ensured to be installed by Mason.
+
+return {
+	"cssls",
+	"cssmodules_ls",
+	"css_variables",
+	"dockerls",
+	-- "eslint_d",
+	"gopls",
+	"html",
+	"htmx",
+	"hyprls", -- For Hyprland config files.
+	"jsonls",
+	"lua_ls",
+	-- "mypy",
+	-- "nginx_language_server",
+	"pyright",
+	-- "prettierd",
+	"pyright",
+	"ruff",
+	"rust_analyzer",
+	"svelte",
+	"tailwindcss",
+	"templ",
+	"ts_ls",
+}
