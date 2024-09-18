8e62364b0aa01f661d1c71c2480723d5e1664e34
Nicholas Oliver Bahary <nicholashendrata110900@gmail.com>
2024-09-19 01:08:57 +0700

    Updated keymap description

--- nvim/lua/keybinds/misc.lua
+++ nvim/lua/keybinds/misc.lua
@@ -13,9 +13,10 @@ vim.keymap.set("n", "<leader>b", ":Neotree<CR>", { desc = "Open the File Tree" }
 vim.keymap.set("n", "<C-p>", builtin.find_files)
 vim.keymap.set("n", "<leader>?", builtin.help_tags, { desc = "Open Help Manual" })
 vim.keymap.set("n", "<leader>p", builtin.find_files, { desc = "Open Telescope" })
-vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Live Grep" })
+vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Live Grep Search" })
+vim.keymap.set("n", "<leader>fc", builtin.grep_string, { desc = "Search Current Word" })
 vim.keymap.set("n", "<leader>fs", function()
-  builtin.grep_string({ search = vim.fn.input("Grep > ") })
+	builtin.grep_string({ search = vim.fn.input("Grep > ") })
 end, { desc = "Search in Files" })
 
 -- Spectre keybindings.
