2141ce0501cb8f3f64e96c89e3392c0a8caf655c
Nicholas Oliver Bahary <nicholashendrata110900@gmail.com>
2024-09-19 07:05:18 +0700

    updated fish ls view

--- fish/aliases.fish
+++ fish/aliases.fish
@@ -1,9 +1,8 @@
 # Replace ls with eza
-alias ls='eza -al --color=always --group-directories-first --icons' # preferred listing
-alias la='eza -a --color=always --group-directories-first --icons'  # all files and dirs
-alias ll='eza -l --color=always --group-directories-first --icons'  # long format
-alias lt='eza -aT --color=always --group-directories-first --icons' # tree listing
-alias l.="eza -a | grep -e '^\.'"                                   # show only dotfiles
+alias ls='eza -al --color=always --group-directories-first --git --icons --no-user --no-time --no-permissions' # preferred listing
+alias la='eza -a --color=always --group-directories-first --git --icons --no-user --no-time --no-permissions'  # all files and dirs
+alias ll='eza -l --color=always --group-directories-first --git --icons --no-user --no-time --no-permissions'  # long format
+alias lt='eza -aT --color=always --group-directories-first --git --icons --no-user --no-time --no-permissions' # tree listing
 
 # Common use
 alias grubup="sudo grub-mkconfig -o /boot/grub/grub.cfg"
