ffdcf18e7a4170f8e8aa3cfe10bac67d4325b4c0
Nicholas Oliver Bahary <nicholashendrata110900@gmail.com>
2024-09-23 04:47:47 +0700

    replaced swaylock with hyprlock

--- rofi/powermenu.sh
+++ rofi/powermenu.sh
@@ -67,15 +67,7 @@ run_cmd() {
 			amixer set Master mute
 			systemctl suspend
 		elif [[ $1 == '--logout' ]]; then
-			if [[ "$DESKTOP_SESSION" == 'openbox' ]]; then
-				openbox --exit
-			elif [[ "$DESKTOP_SESSION" == 'bspwm' ]]; then
-				bspc quit
-			elif [[ "$DESKTOP_SESSION" == 'i3' ]]; then
-				i3-msg exit
-			elif [[ "$DESKTOP_SESSION" == 'plasma' ]]; then
-				qdbus org.kde.ksmserver /KSMServer logout 0 0 0
-			fi
+			hyprctl dispatch exit
 		fi
 	else
 		exit 0
@@ -92,7 +84,7 @@ case ${chosen} in
 		run_cmd --reboot
         ;;
     $lock)
-		swaylock
+		hyprlock
         ;;
     $suspend)
 		run_cmd --suspend
