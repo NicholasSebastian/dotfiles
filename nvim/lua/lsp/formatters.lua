28c529d81ca36c3c5518c089d4805880c543e1a7
Nicholas Oliver Bahary <nicholashendrata110900@gmail.com>
2024-09-24 22:56:44 +0700

    updated eslint config file include list

--- nvim/lua/lsp/formatters.lua
+++ nvim/lua/lsp/formatters.lua
@@ -4,40 +4,40 @@
 local null_ls = require("null-ls")
 
 local eslint_config = {
-  condition = function(utils)
-    return utils.root_has_file({
-      ".eslintrc",
-      ".eslintrc.js",
-      ".eslintrc.cjs",
-      ".eslintrc.yaml",
-      ".eslintrc.yml",
-      ".eslintrc.json",
-    })
-  end,
+	condition = function(utils)
+		return utils.root_has_file({
+			"eslint.config.js",
+			"eslint.config.mjs",
+			"eslint.config.cjs",
+			"eslint.config.ts",
+			"eslint.config.mts",
+			"eslint.config.cts",
+		})
+	end,
 }
 
 return {
-  -- Lua
-  null_ls.builtins.formatting.stylua,
+	-- Lua
+	null_ls.builtins.formatting.stylua,
 
-  -- JavaScript
-  null_ls.builtins.formatting.prettierd.with({ extra_args = { "--trailing-comma", "none" } }),
-  require("none-ls.diagnostics.eslint").with(eslint_config),
-  require("none-ls.code_actions.eslint").with(eslint_config),
+	-- JavaScript
+	null_ls.builtins.formatting.prettierd.with({ extra_args = { "--trailing-comma", "none" } }),
+	require("none-ls.diagnostics.eslint").with(eslint_config),
+	require("none-ls.code_actions.eslint").with(eslint_config),
 
-  -- GoLang
-  null_ls.builtins.formatting.gofmt,
-  null_ls.builtins.formatting.goimports, -- go install golang.org/x/tools/cmd/goimports@latest
+	-- GoLang
+	null_ls.builtins.formatting.gofmt,
+	null_ls.builtins.formatting.goimports, -- go install golang.org/x/tools/cmd/goimports@latest
 
-  -- Python
-  null_ls.builtins.formatting.black,
-  null_ls.builtins.formatting.isort,
-  null_ls.builtins.diagnostics.mypy,
-  require("none-ls.diagnostics.ruff"),
+	-- Python
+	null_ls.builtins.formatting.black,
+	null_ls.builtins.formatting.isort,
+	null_ls.builtins.diagnostics.mypy,
+	require("none-ls.diagnostics.ruff"),
 
-  -- Tailwind
-  null_ls.builtins.formatting.rustywind,
+	-- Tailwind
+	null_ls.builtins.formatting.rustywind,
 
-  -- Git
-  null_ls.builtins.code_actions.gitsigns,
+	-- Git
+	null_ls.builtins.code_actions.gitsigns,
 }
