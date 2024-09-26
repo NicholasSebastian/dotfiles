c72c65cd542b830e043ced8817877544b18b538d
Nicholas Oliver Bahary <nicholashendrata110900@gmail.com>
2024-09-27 02:00:19 +0700

    code splitting and renaming

---
+++ nvim/lua/keybinds/overseer.lua
@@ -0,0 +1,37 @@
+return {
+  {
+    "<leader>ow",
+    "<cmd>OverseerToggle<cr>",
+    desc = "Task list",
+  },
+  {
+    "<leader>oo",
+    "<cmd>OverseerRun<cr>",
+    desc = "Run task",
+  },
+  {
+    "<leader>oq",
+    "<cmd>OverseerQuickAction<cr>",
+    desc = "Action recent task",
+  },
+  {
+    "<leader>oi",
+    "<cmd>OverseerInfo<cr>",
+    desc = "Overseer Info",
+  },
+  {
+    "<leader>ob",
+    "<cmd>OverseerBuild<cr>",
+    desc = "Task builder",
+  },
+  {
+    "<leader>ot",
+    "<cmd>OverseerTaskAction<cr>",
+    desc = "Task action",
+  },
+  {
+    "<leader>oc",
+    "<cmd>OverseerClearCache<cr>",
+    desc = "Clear cache",
+  },
+}
--- nvim/lua/plugins/editor.lua
+++ nvim/lua/plugins/editor.lua
@@ -1,72 +1,70 @@
 -- https://github.com/folke/which-key.nvim
 -- https://github.com/folke/trouble.nvim
 
-local keybinds = require("keybinds/editor")
-
 return {
-	{
-		-- Cheatsheet for all the keybindings.
-		"folke/which-key.nvim",
-		event = "VeryLazy",
-		opts = {
-			preset = "modern",
-			spec = require("cheatsheet"),
-			triggers = {
-				{ "<auto>", mode = "nixsotc" },
-			},
-		},
-	},
-	{
-		-- Project errors and warnings panel.
-		"folke/trouble.nvim",
-		opts = {},
-		cmd = "Trouble",
-		keys = keybinds.trouble,
-	},
-	{
-		-- Command Runner window.
-		"stevearc/overseer.nvim",
-		keys = keybinds.overseer,
-		cmd = {
-			"OverseerOpen",
-			"OverseerClose",
-			"OverseerToggle",
-			"OverseerSaveBundle",
-			"OverseerLoadBundle",
-			"OverseerDeleteBundle",
-			"OverseerRunCmd",
-			"OverseerRun",
-			"OverseerInfo",
-			"OverseerBuild",
-			"OverseerQuickAction",
-			"OverseerTaskAction",
-			"OverseerClearCache",
-		},
-		opts = {
-			dap = false,
-			form = {
-				win_opts = {
-					winblend = 0,
-				},
-			},
-			confirm = {
-				win_opts = {
-					winblend = 0,
-				},
-			},
-			task_win = {
-				win_opts = {
-					winblend = 0,
-				},
-			},
-			task_list = {
-				bindings = {
-					["<C-h>"] = false,
-					["<C-j>"] = false,
-					["<C-k>"] = false,
-					["<C-l>"] = false,
-				},
-			},
-		},
-	},
+  {
+    -- Cheatsheet for all the keybindings.
+    "folke/which-key.nvim",
+    event = "VeryLazy",
+    opts = {
+      preset = "modern",
+      spec = require("cheatsheet"),
+      triggers = {
+        { "<auto>", mode = "nixsotc" },
+      },
+    },
+  },
+  {
+    -- Project errors and warnings panel.
+    "folke/trouble.nvim",
+    opts = {},
+    cmd = "Trouble",
+    keys = require("keybinds/trouble"),
+  },
+  {
+    -- Command Runner window.
+    "stevearc/overseer.nvim",
+    keys = require("keybinds/overseer"),
+    cmd = {
+      "OverseerOpen",
+      "OverseerClose",
+      "OverseerToggle",
+      "OverseerSaveBundle",
+      "OverseerLoadBundle",
+      "OverseerDeleteBundle",
+      "OverseerRunCmd",
+      "OverseerRun",
+      "OverseerInfo",
+      "OverseerBuild",
+      "OverseerQuickAction",
+      "OverseerTaskAction",
+      "OverseerClearCache",
+    },
+    opts = {
+      dap = false,
+      form = {
+        win_opts = {
+          winblend = 0,
+        },
+      },
+      confirm = {
+        win_opts = {
+          winblend = 0,
+        },
+      },
+      task_win = {
+        win_opts = {
+          winblend = 0,
+        },
+      },
+      task_list = {
+        bindings = {
+          ["<C-h>"] = false,
+          ["<C-j>"] = false,
+          ["<C-k>"] = false,
+          ["<C-l>"] = false,
+        },
+      },
+    },
+  },
 }
