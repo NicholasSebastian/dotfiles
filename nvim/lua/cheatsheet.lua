65dd65e4348b8fac08277144632d2c7a6b4d2db9
Nicholas Oliver Bahary <nicholashendrata110900@gmail.com>
2024-09-27 04:10:26 +0700

    added an entry to the cheatsheet

--- nvim/lua/cheatsheet.lua
+++ nvim/lua/cheatsheet.lua
@@ -1,6 +1,7 @@
 return {
 	{ "<leader>c", group = " Code Actions" },
 	{ "<leader>f", group = " Search" },
+	{ "<leader>g", group = " Git Actions" },
 	{ "<leader>r", group = " Refactor" },
 	{ "<leader>o", group = " Overseer" },
 	{ "<leader>s", group = " Window Actions" },
