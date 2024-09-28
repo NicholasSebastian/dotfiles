0b7f8d9226bd37599e129c9bdf67646a441b2cfa
Nicholas Oliver Bahary <nicholashendrata110900@gmail.com>
2024-09-29 02:50:11 +0700

    fixed neotree options not getting loaded

--- nvim/lua/plugins/neotree.lua
+++ nvim/lua/plugins/neotree.lua
@@ -15,32 +15,33 @@ return {
 		"nvim-tree/nvim-web-devicons",
 		"MunifTanjim/nui.nvim",
 	},
-	opts = {
-		sources = { "filesystem", "buffers", "git_status" },
-		close_if_last_window = true,
-		window = {
-			position = "left",
-			width = 40,
-		},
-		filesystem = {
-			filtered_items = {
-				visible = true,
-				show_hidden_count = true,
-				hide_dotfiles = false,
-				hide_gitignored = false,
-				hide_by_name = {},
-				never_show = {
-					".git",
-					".DS_Store",
-					".Trash-1000",
-					"thumbs.db",
-					"$RECYCLE.BIN",
-					"System Volume Information",
+	config = function()
+		require("neo-tree").setup({
+			sources = { "filesystem", "buffers", "git_status" },
+			close_if_last_window = true,
+			window = {
+				position = "left",
+				width = 40,
+			},
+			filesystem = {
+				filtered_items = {
+					visible = true,
+					show_hidden_count = true,
+					hide_dotfiles = false,
+					hide_gitignored = false,
+					hide_by_name = {},
+					never_show = {
+						".git",
+						".DS_Store",
+						".Trash-1000",
+						"thumbs.db",
+						"$RECYCLE.BIN",
+						"System Volume Information",
+					},
 				},
 			},
-		},
-	},
-	config = function()
+		})
+
 		-- Open Neotree on launch and on new tabs.
 		open_neotree()
 		vim.api.nvim_create_autocmd("TabNew", { pattern = "*", callback = open_neotree })
