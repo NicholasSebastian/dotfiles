bc08fd04649b9ef8f66c87c7a5a0f9e88ee008ec
Nicholas Oliver Bahary <nicholashendrata110900@gmail.com>
2024-10-05 23:10:02 +0700

    added option to ignore @ rules for CSS

--- nvim/lua/lsp/lsp-config.lua
+++ nvim/lua/lsp/lsp-config.lua
@@ -11,10 +11,9 @@ config.svelte.setup({ capabilities = autocomplete })
 config.templ.setup({ capabilities = autocomplete })
 config.emmet_language_server.setup({ capabilities = autocomplete })
 
--- CSS, SCSS, Sass, and Tailwind.
-config.cssls.setup({ capabilities = autocomplete })
-config.cssmodules_ls.setup({ capabilities = autocomplete })
+-- CSS Variables, CSS Modules, SCSS, Sass, and Tailwind.
 config.css_variables.setup({ capabilities = autocomplete })
+config.cssmodules_ls.setup({ capabilities = autocomplete })
 config.somesass_ls.setup({ capabilities = autocomplete })
 config.tailwindcss.setup({ capabilities = autocomplete })
 
@@ -48,131 +47,143 @@ config.nginx_language_server.setup({ capabilities = autocomplete })
 -- Hyprland config file.
 config.hyprls.setup({ capabilities = autocomplete })
 
+-- CSS.
+config.cssls.setup({
+	capabilities = autocomplete,
+	settings = {
+		css = {
+			lint = {
+				unknownAtRules = "ignore",
+			},
+		},
+	},
+})
+
 -- Lua.
 config.lua_ls.setup({
-  capabilities = autocomplete,
-  settings = {
-    Lua = {
-      diagnostics = {
-        enable = false, -- Let selene handle linting.
-      },
-    },
-  },
+	capabilities = autocomplete,
+	settings = {
+		Lua = {
+			diagnostics = {
+				enable = false, -- Let selene handle linting.
+			},
+		},
+	},
 })
 
 -- Golang.
 config.gopls.setup({
-  capabilities = autocomplete,
-  cmd = { "gopls" },
-  filetypes = { "go", "gomod", "gowork", "gotmpl" },
-  root_dir = util.root_pattern("go.work", "go.mod", ".git"),
-  settings = {
-    gopls = {
-      gofumpt = true,
-      codelenses = {
-        gc_details = false,
-        generate = true,
-        regenerate_cgo = true,
-        run_govulncheck = true,
-        test = true,
-        tidy = true,
-        upgrade_dependency = true,
-        vendor = true,
-      },
-      hints = {
-        assignVariableTypes = true,
-        compositeLiteralFields = true,
-        compositeLiteralTypes = true,
-        constantValues = true,
-        functionTypeParameters = true,
-        parameterNames = true,
-        rangeVariableTypes = true,
-      },
-      analyses = {
-        fieldalignment = true,
-        nilness = true,
-        unusedparams = true,
-        unusedwrite = true,
-        useany = true,
-      },
-      usePlaceholders = true,
-      completeUnimported = true,
-      staticcheck = true,
-      semanticTokens = true,
-    },
-  },
-  on_attach = function(client, _)
-    -- workaround for gopls not supporting semanticTokensProvider
-    -- https://github.com/golang/go/issues/54531#issuecomment-1464982242
-    if not client.server_capabilities.semanticTokensProvider then
-      local semantic = client.config.capabilities.textDocument.semanticTokens
-      client.server_capabilities.semanticTokensProvider = {
-        full = true,
-        range = true,
-        legend = {
-          tokenTypes = semantic.tokenTypes,
-          tokenModifiers = semantic.tokenModifiers,
-        },
-      }
-    end
-  end,
+	capabilities = autocomplete,
+	cmd = { "gopls" },
+	filetypes = { "go", "gomod", "gowork", "gotmpl" },
+	root_dir = util.root_pattern("go.work", "go.mod", ".git"),
+	settings = {
+		gopls = {
+			gofumpt = true,
+			codelenses = {
+				gc_details = false,
+				generate = true,
+				regenerate_cgo = true,
+				run_govulncheck = true,
+				test = true,
+				tidy = true,
+				upgrade_dependency = true,
+				vendor = true,
+			},
+			hints = {
+				assignVariableTypes = true,
+				compositeLiteralFields = true,
+				compositeLiteralTypes = true,
+				constantValues = true,
+				functionTypeParameters = true,
+				parameterNames = true,
+				rangeVariableTypes = true,
+			},
+			analyses = {
+				fieldalignment = true,
+				nilness = true,
+				unusedparams = true,
+				unusedwrite = true,
+				useany = true,
+			},
+			usePlaceholders = true,
+			completeUnimported = true,
+			staticcheck = true,
+			semanticTokens = true,
+		},
+	},
+	on_attach = function(client, _)
+		-- workaround for gopls not supporting semanticTokensProvider
+		-- https://github.com/golang/go/issues/54531#issuecomment-1464982242
+		if not client.server_capabilities.semanticTokensProvider then
+			local semantic = client.config.capabilities.textDocument.semanticTokens
+			client.server_capabilities.semanticTokensProvider = {
+				full = true,
+				range = true,
+				legend = {
+					tokenTypes = semantic.tokenTypes,
+					tokenModifiers = semantic.tokenModifiers,
+				},
+			}
+		end
+	end,
 })
 
 -- Rust.
 config.rust_analyzer.setup({
-  capabilities = autocomplete,
-  filetypes = { "rust" },
-  root_dir = util.root_pattern("Cargo.toml"),
-  settings = {
-    ["rust-analyzer"] = {
-      cargo = {
-        allFeatures = true,
-      },
-    },
-  },
+	capabilities = autocomplete,
+	filetypes = { "rust" },
+	root_dir = util.root_pattern("Cargo.toml"),
+	settings = {
+		["rust-analyzer"] = {
+			cargo = {
+				allFeatures = true,
+			},
+		},
+	},
 })
 
 -- C-Sharp.
 config.omnisharp.setup({
-  enable_roslyn_analyzers = true,
-  organize_imports_on_format = true,
-  enable_import_completion = true,
-  handlers = {
-    ["textDocument/definition"] = function(...)
-      return require("omnisharp_extended").handler(...)
-    end,
-  },
-  keys = keybinds.csharp,
+	enable_roslyn_analyzers = true,
+	organize_imports_on_format = true,
+	enable_import_completion = true,
+	handlers = {
+		["textDocument/definition"] = function(...)
+			return require("omnisharp_extended").handler(...)
+		end,
+	},
+	keys = keybinds.csharp,
 })
 
 -- JSON.
 config.jsonls.setup({
-  capabilities = autocomplete,
-  settings = {
-    json = {
-      schemas = schemastore.json.schemas(),
-      validate = { enable = true },
-      format = { enable = true },
-    },
-  },
+	capabilities = autocomplete,
+	settings = {
+		json = {
+			schemas = schemastore.json.schemas(),
+			validate = { enable = true },
+			format = { enable = true },
+		},
+	},
 })
 
 -- YAML.
 config.yamlls.setup({
-  capabilities = autocomplete,
-  settings = {
-    redhat = {
-      telemetry = { enabled = false },
-    },
-    yaml = {
-      schemas = schemastore.yaml.schemas(),
-      validate = true,
-      format = { enable = true },
-      keyOrdering = false,
-      schemaStore = {
-        enable = false,
-        url = "",
-      },
-    },
-  },
+	capabilities = autocomplete,
+	settings = {
+		redhat = {
+			telemetry = { enabled = false },
+		},
+		yaml = {
+			schemas = schemastore.yaml.schemas(),
+			validate = true,
+			format = { enable = true },
+			keyOrdering = false,
+			schemaStore = {
+				enable = false,
+				url = "",
+			},
+		},
+	},
 })
