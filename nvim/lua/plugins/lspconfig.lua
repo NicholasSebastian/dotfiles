2044ca7e47b57035a17b11b5f82c0f7b925e44af
Nicholas Oliver Bahary <nicholashendrata110900@gmail.com>
2024-10-17 02:55:47 +0700

    disabled inlay hints for typescript

--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -1,11 +1,21 @@
 return {
   "neovim/nvim-lspconfig",
-  opts = function()
-    local keys = require("lazyvim.plugins.lsp.keymaps").get()
-
-    -- Remap some of the default LSP keymaps
-    keys[#keys + 1] = { "<C-space>", vim.lsp.buf.hover }
-    keys[#keys + 1] = { "<C-k>", vim.lsp.buf.signature_help }
-    keys[#keys + 1] = { "K", false }
-  end,
+  opts = {
+    servers = {
+      vtsls = {
+        settings = {
+          typescript = {
+            inlayHints = { -- Disable inlay hints cause they're kinda annoying.
+              enumMemberValues = { enabled = false },
+              functionLikeReturnTypes = { enabled = false },
+              parameterNames = { enabled = "none" },
+              parameterTypes = { enabled = false },
+              propertyDeclarationTypes = { enabled = false },
+              variableTypes = { enabled = false },
+            },
+          },
+        },
+      },
+    },
+  },
 }
