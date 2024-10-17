00b3d583a011d91721c2e09cb0e6198666577c76
Nicholas Oliver Bahary <nicholashendrata110900@gmail.com>
2024-10-17 15:56:57 +0700

    removed dadbod

---
+++ nvim/lua/plugins/vim-dadbod.lua
@@ -0,0 +1,8 @@
+-- Disable dadbod cause I don't use it.
+-- I just want the default formatter and linter configurations for SQL.
+
+return {
+  { "tpope/vim-dadbod", enabled = false },
+  { "kristijanhusak/vim-dadbod-completion", enabled = false },
+  { "kristijanhusak/vim-dadbod-ui", enabled = false },
+}
